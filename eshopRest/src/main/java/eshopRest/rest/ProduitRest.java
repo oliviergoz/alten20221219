92118cbed783f1d77d16ab3f283cc1c62ca2fc61
olivier <o.gozlan@ajc-ingenierie.fr>
2022-12-23 12:26:40 +0100

    ajout PUT et DELETE

--- eshopRest/src/main/java/eshopRest/rest/ProduitRest.java
+++ eshopRest/src/main/java/eshopRest/rest/ProduitRest.java
@@ -3,8 +3,10 @@ package eshopRest.rest;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -38,23 +40,44 @@ public class ProduitRest {
 //		}
 //		return p;
 //	}
-	
+
 	@GET
 	@Path("/{id}")
 	public Response getProduitById(@PathParam("id") Long id) {
-		Produit p=daoProduit.findByKey(id);
-		if(p==null) {
-			throw new WebApplicationException(Response.Status.NOT_FOUND);
-		}
-		return Response.ok(p).build();
+		return Response.ok(findById(id)).build();
 	}
-	
-	//creation
+
+	// creation
 	@POST
 	@Path("")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Produit createProduit(Produit produit) {
 		daoProduit.insert(produit);
 		return produit;
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public void deleteById(@PathParam("id") Long id) {
+		daoProduit.delete(findById(id));
+	}
+
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Produit update(@PathParam("id") Long id, Produit produit) {
+		Produit produitEnBase = findById(id);
+		produitEnBase.setLibelle(produit.getLibelle());
+		produitEnBase.setDescription(produit.getDescription());
+		produitEnBase.setPrix(produit.getPrix());
+		return daoProduit.update(produitEnBase);
+	}
+
+	private Produit findById(Long id) {
+		Produit p = daoProduit.findByKey(id);
+		if (p == null) {
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+		return p;
 	}
 }
