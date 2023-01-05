599677e947e3b6f304bfdceeb2018479e0979188
olivier <o.gozlan@ajc-ingenierie.fr>
2023-01-05 12:55:47 +0100

    ajout parametre

--- formationSpringMvc/src/main/java/formationSpringMvc/controller/PublicController.java
+++ formationSpringMvc/src/main/java/formationSpringMvc/controller/PublicController.java
@@ -3,13 +3,15 @@ package formationSpringMvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/public")
 public class PublicController {
 
 	@GetMapping("")
-	public String home() {
+	public String home(@RequestParam String prenom) {
+		System.out.println(prenom);
 		return "public/home";
 	}
 }
