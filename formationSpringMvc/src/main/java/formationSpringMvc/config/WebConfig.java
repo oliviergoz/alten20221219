2c6909217415eda82814bfe4f7182ea2ef093f70
olivier <o.gozlan@ajc-ingenierie.fr>
2023-01-05 12:43:19 +0100

    projet spring mvc

---
+++ formationSpringMvc/src/main/java/formationSpringMvc/config/WebConfig.java
@@ -0,0 +1,31 @@
+package formationSpringMvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "formationSpringMvc.controller")
+public class WebConfig implements WebMvcConfigurer {
+
+	// definition de mapping accessible directement sans passer par un controller
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+	}
+
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
