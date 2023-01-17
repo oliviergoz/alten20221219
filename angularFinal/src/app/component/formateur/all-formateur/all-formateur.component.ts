5df207c30c0f7bceaa4568bc275463c69ab943ef
olivier <o.gozlan@ajc-ingenierie.fr>
2023-01-17 12:22:56 +0100

    service formateur

--- angularFinal/src/app/component/formateur/all-formateur/all-formateur.component.ts
+++ angularFinal/src/app/component/formateur/all-formateur/all-formateur.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { FormateurService } from './../../../services/formateur.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-all-formateur',
   templateUrl: './all-formateur.component.html',
-  styleUrls: ['./all-formateur.component.css']
+  styleUrls: ['./all-formateur.component.css'],
 })
-export class AllFormateurComponent {
-
+export class AllFormateurComponent implements OnInit {
+  formateurs: any[] = [];
+  constructor(private formateurService: FormateurService) {}
+  ngOnInit(): void {
+    this.formateurService.getAll().subscribe((data: any[]) => {
+      this.formateurs = data;
+    });
+  }
 }
